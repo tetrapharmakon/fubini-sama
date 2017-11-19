e133ca2e955e1523ddb5017e46a4a8d614fde121
killing_buddha <tetrapharmakon@gmail.com>
2017-11-20 00:20:29 +0100

    "it's a cruel world"

---
+++ sections/auto/00-prelims.el
@@ -0,0 +1,13 @@
+(TeX-add-style-hook
+ "00-prelims"
+ (lambda ()
+   (LaTeX-add-labels
+    "a-few-isos"
+    "cofri.ninja"
+    "cofri.kan"
+    "cofri.yan"
+    "def:wcolim"
+    "wlim-are-coends"
+    "all-are"))
+ :latex)
+
